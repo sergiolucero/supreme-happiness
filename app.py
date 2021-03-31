1acb1d7c00df9ccca61701709d9336cab2d9aaa4
Ubuntu <ubuntu@ip-172-26-2-243.ec2.internal>
2021-03-31 22:59:46 +0000

    Flask+CSS

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, make_response, render_template, request, jsonify
 from flask_cors import CORS
 import sqlite3, pandas as pd
+
 sql = lambda q: pd.read_sql(q, sqlite3.connect('greenpeace.db'))
 
 app = Flask(__name__, static_folder='static')
@@ -10,8 +11,12 @@ cors = CORS(app)
 def hello_world():
     #men = pd.read_csv('keywords.txt', names=['Tema'])
     #men['Tema']=men.Tema.apply(lambda t: t.split('\t')[1])
-    men = sql('SELECT * FROM menciones')
-    return render_template('menciones.html', data=men.to_html(index=False))
+    men = sql('SELECT * FROM menciones ORDER BY menciones DESC')
+    men['link'] = ['<A HREF="%s">click</A>' %link for link in men['link']]
+
+    data = men.to_html(index=False, escape=False, classes='mystyle')
+
+    return render_template('menciones.html', data=data)
 
 if __name__ == '__main__':
     app.run(port=8081, debug=True, host='0.0.0.0')
