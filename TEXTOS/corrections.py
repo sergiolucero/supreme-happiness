4ac086725bfe7f88a684a5bb63280c70db23b425
Ubuntu <ubuntu@ip-172-26-2-243.ec2.internal>
2021-04-02 13:02:43 +0000

    MILESTONE: removed spaces

--- TEXTOS/corrections.py
+++ TEXTOS/corrections.py
@@ -1,7 +1,10 @@
-import glob
+import glob, time
 
+t0=time.time()
 for fn in glob.glob('TODOS/*.txt'):
-    text=open(fn).read().replace('\\n','').replace('\n','').replace('\t','')
+    text=open(fn).read().replace('\\n','').replace('\n','').replace('\t','').replace("' '",'')
     fw = open(fn,'w')
     fw.write(text)
     fw.close()
+
+print('DT:', round(time.time()-t0,2))
