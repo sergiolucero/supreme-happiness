2ed28980ad79100f14fe90d72305ded5c2d04c5f
Ignacio Norambuena <inorambuenaa@gmail.com>
2021-04-22 19:30:56 +0000

    fixed link menciones

--- conceptos.py
+++ conceptos.py
@@ -19,7 +19,7 @@ for tabla, files in pfiles.items():
         smen = smen.append(finders(textos, concepto, cmenciones))
 
     #BASE= 'http://greenpeace.quant.cl:8081'
-    BASE= 'http://http://greenpeace-monitor.herokuapp.com'
+    BASE= 'http://greenpeace-monitor.herokuapp.com'
     smen['link'] = smen.concepto.apply(lambda concepto: '%s/ver_menciones/%s/50' %(BASE,concepto))
     smen.to_sql(f'menciones_{tabla}', conn, index=False, if_exists='replace')
 
