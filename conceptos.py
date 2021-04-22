ce6d16108086a79b59e1dd2cb485d1ed9fc37746
Ignacio Norambuena <inorambuenaa@gmail.com>
2021-04-22 19:13:25 +0000

    MILESTONE: keywords_final processed

--- conceptos.py
+++ conceptos.py
@@ -1,9 +1,7 @@
 import sqlite3, pandas as pd
 import glob
-import time
-import re
+from searcher import finders
 
-matches = lambda wt: len(list(re.finditer(wt[0].lower(), wt[1])))
 conn=sqlite3.connect('greenpeace.db')
 
 pfiles = {'todos': list(glob.glob('TEXTOS/TODOS/*.txt')),
@@ -11,32 +9,18 @@ pfiles = {'todos': list(glob.glob('TEXTOS/TODOS/*.txt')),
 }
 
 for tabla, files in pfiles.items():
-    men = pd.read_csv('keywords2.txt', header=0)  # redone 21/04 concepto, tema
-    nProgramas = len(files)
-    print('nProgramas:', nProgramas)
-
-    t0 = time.time()
+    kw = eval(open('keywords_final.txt').read())  # last train
     textos=[open(fn).read() for fn in files]
-    print('DT:', round(time.time()-t0,2))  # 0.11 sec
 
-    nmatches=[]
-    amatches=[]
-    print('TEMAS:', men['tema'])
-    smen = pd.DataFrame(men['concepto'].unique(), columns=['concepto'])
+    smen = pd.DataFrame()
+    for concepto, menciones in kw.items():
+        cmenciones = [concepto]+menciones
+        # primero buscamos el concepto
+        smen = smen.append(finders(textos, concepto, cmenciones))
 
-    for word, cdf in men.groupby('concepto'):
-        wnmatch = 0;         wamatch = 0.0
-        for _, row in cdf.iterrows():
-            print('CONCEPTO:', word, 'TEMA:', row['tema'])
-            wnmatch += sum(matches([row['tema'],text]) for text in textos)   # missing space?
-            wamatch += sum([1 if row['tema'] in text else 0 for text in textos])/nProgramas    # normalized 20/04
-        nmatches.append(wnmatch);        amatches.append(wamatch)
-    print('NA', nmatches, amatches)
-    smen['nMenciones']=nmatches
-    smen['\% Mencionan']=[round(100*amat,2) for amat in amatches]
-
-    BASE= 'http://greenpeace.quant.cl:8081'
-    smen['link']=men.concepto.apply(lambda concepto: '%s/ver_menciones/%s/50' %(BASE,concepto))
+    #BASE= 'http://greenpeace.quant.cl:8081'
+    BASE= 'http://http://greenpeace-monitor.herokuapp.com'
+    smen['link'] = smen.concepto.apply(lambda concepto: '%s/ver_menciones/%s/50' %(BASE,concepto))
     smen.to_sql(f'menciones_{tabla}', conn, index=False, if_exists='replace')
 
-    print(tabla); print(men)
+    print(tabla); print(smen)
--- searcher.py
+++ searcher.py
@@ -2,6 +2,8 @@ import glob, re
 import sqlite3, pandas as pd
 
 sql = lambda q: pd.read_sql(q, sqlite3.connect('greenpeace.db'))
+matches = lambda wt: len(list(re.finditer(wt[0].lower(), wt[1])))
+
 
 def cubicalo(tipo):     # proyecciones bidimensionales: (i) por distrito/lista/partido (ii) totales/absolutas
 
@@ -31,3 +33,25 @@ def querier(query, WIDTH=80, tipo=None):
                                 for f in fmatches]
 
     return matches
+
+
+def finders(textos, concepto, menciones):
+
+    conceptos = [concepto]*len(menciones)
+    smen = pd.DataFrame(dict(concepto=conceptos, mencion=menciones))
+    nProgramas = len(textos)
+
+    nmatches=[]
+    amatches=[]
+
+    for concepto, mencion in zip(conceptos, menciones):
+        wnmatch = 0;         wamatch = 0.0
+        print('CONCEPTO:', concepto, 'MENCION:', mencion)
+        wnmatch += sum(matches([mencion,text]) for text in textos)   # missing space?
+        wamatch += sum([1 if mencion in text else 0 for text in textos])/nProgramas    # normalized 20/04
+        nmatches.append(wnmatch);        amatches.append(wamatch)
+
+    smen['nMenciones']=nmatches
+    smen['\% Mencionan']=[round(100*amat,2) for amat in amatches]
+
+    return smen
