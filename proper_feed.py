e479f0cf569e6de1c1622d4dd642d063bfe2def4
Ignacio Norambuena <inorambuenaa@gmail.com>
2021-04-28 15:27:05 +0000

    proper_feed

--- proper_feed.py
+++ proper_feed.py
@@ -10,9 +10,15 @@ ori = [(kwk,querier(kwk,tipo='ori')) for kwk,kwv in kw.items()]
 df = pd.DataFrame(dict(concepto=[x[0] for x in non]))
 df['nMenciones'] = [x[1][1] for x in non]
 df['nProgramas'] = [len(x[1][0]) for x in non]
+n1 = len(glob.glob('TEXTOS/TODOS/D*.txt'))
+print('N1:',n1)
+df['Porc.Programas'] = round(100*df['nProgramas']/n1,2)
 df.to_sql('menciones_todos', conn, index=False, if_exists='replace')
 
 dfo = pd.DataFrame(dict(concepto=[x[0] for x in ori]))
 dfo['nMenciones'] = [x[1][1] for x in ori]
 dfo['nProgramas'] = [len(x[1][0]) for x in ori]
+n2 = len(glob.glob('TEXTOS/INDIGENAS/*.txt'))
+print('N2:', n2)
+dfo['Porc.Programas'] = round(100*dfo['nProgramas']/n2,2)
 dfo.to_sql('menciones_ori', conn, index=False, if_exists='replace')
