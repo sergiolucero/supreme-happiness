5cbf2951108f3146f4c0817f6be5a6117293cf9d
Sergio Lucero <sergiolucero@gmail.com>
2021-05-01 21:15:38 +0000

    FIXING copiones

--- copiones.py
+++ copiones.py
@@ -1,7 +1,8 @@
-import glob
+import glob, operator
 import pandas as pd
 from pprint import pprint
-
+from collections import OrderedDict
+##############################################
 files=glob.glob('TEXTOS/TODOS/*.txt')
 textos=[open(fn).read() for fn in files]
 stext=set(textos)
@@ -10,25 +11,30 @@ lendick={len(k):len(v) for k,v in clus.items()}
 longdicks={len(k): v for k,v in clus.items() if len(v)>1}
 
 df=pd.read_csv('candidatos.csv')
-fw=open('static/copiones.html','w')
 
 ix=0;lens = {}
 for k,v in longdicks.items():
     ix+=1; n=len(v); # nOffenders
-    pprint(f'<H3>Cluster {ix} ({n} programas idénticos)</H3>', fw)
     vfiles=[files[ix] for ix in v]
     #vdf=??
     vx = [f.split('/')[-1].replace('_',' ') for f in vfiles]
     clist = [(' '.join(c.split()[1:]))[:-4] for c in vx]  # DX JUAN VERA CARRASCO.txt
     vdf = df[df.candidato.isin(clist)]
     vdf = vdf[['candidato','distrito','partido','lista']]
-    vdf.to_html(fw, index=False,escape=True)
     partidos = vdf.partido.value_counts()
     listas = vdf.lista.value_counts()
-    lens[ix] = (n,partidos.to_dict(),listas)
-    pprint('<HR>',fw)
+    lens[ix] = (n,partidos.to_dict(),listas,vdf)
     print(k,len(clist))
 
+lens =  sorted(lens.items(), key=lambda x:x[0], reverse=True)
+lens = OrderedDict(lens)
+
+fw=open('static/copiones.html','w')
+for cidx, clus in lens.items():
+    n, pdict, listas, vdf = clus
+    pprint(f'<H3>Cluster {cidx} ({n} programas idénticos)</H3>', fw)
+    pprint('<HR>',fw)
+    vdf.to_html(fw, index=False,escape=True)
 fw.close()
 
 lv=list(lens.values())
